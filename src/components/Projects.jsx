3b66457b6ca42cffb89948bb013656a4ec319eb0
omenihu victor <you@example.com>
2025-10-27 01:14:12 +0100

    live

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from 'react'
 import ScreenshotOne from './image/Screenshot (31).png'
 import ScreenshotTwo from './image/Screenshot (4).png'
+import ScreenshotThree from './image/Screenshot (5).png'
+import ScreenshotFour from './image/Screenshot (8).png'
 
 const Projects = () => {
   const scrollRef = useRef(null);
@@ -105,6 +107,88 @@ const Projects = () => {
 
             <a 
               href="https://movix-delta-teal.vercel.app/"
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button className='font-bold border-[1px] border-solid border-gray-400 p-2 rounded-4xl
+                transition-all ease-in-out duration-500 hover:-translate-y-1.5 cursor-pointer
+                active:translate-0 ml-6 mt-4 opacity-80
+              '>
+                Live Demo
+              </button>
+            </a>
+          </div>
+        </div>
+        
+        <div className='mb-6 ml-4 mt-[60px] flex-shrink-3 min-w-[400px] border-[1px] border-solid border-gray-200
+         text-black font-semibold hover:opacity-80
+          rounded-2xl shadow-md p-6
+        '>
+          <img 
+            src={ScreenshotThree}
+            alt="" 
+            className='rounded-lg'
+          />
+          <h1 className='text-2xl text-center mt-6'>
+            MOVIX LOGIN PAGE
+          </h1>
+
+          <div className='justify-center flex flex-row items-center'>
+            <a 
+              href="https://github.com/Vf-projects/Movix-Dashboard/tree/main/LOGIN"
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button className='font-bold border-[1px] border-solid border-gray-400 p-2 rounded-4xl
+                transition-all ease-in-out duration-500 hover:-translate-y-1.5 cursor-pointer opacity-80
+                active:translate-0 mt-4
+              '>
+                Github
+              </button>
+            </a>
+
+            <a 
+              href="https://movix-login.vercel.app/"
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button className='font-bold border-[1px] border-solid border-gray-400 p-2 rounded-4xl
+                transition-all ease-in-out duration-500 hover:-translate-y-1.5 cursor-pointer
+                active:translate-0 ml-6 mt-4 opacity-80
+              '>
+                Live Demo
+              </button>
+            </a>
+          </div>
+        </div><div className='mb-6 ml-4 mt-[60px] flex-shrink-3 min-w-[400px] border-[1px] border-solid border-gray-200
+         text-black font-semibold hover:opacity-80
+          rounded-2xl shadow-md p-6
+        '>
+          <img 
+            src={ScreenshotFour}
+            alt="" 
+            className='rounded-lg'
+          />
+          <h1 className='text-2xl text-center mt-6'>
+            MOVIX REGISTER PAGE
+          </h1>
+
+          <div className='justify-center flex flex-row items-center'>
+            <a 
+              href="https://github.com/Vf-projects/Movix-Dashboard/tree/main/REGISTER"
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <button className='font-bold border-[1px] border-solid border-gray-400 p-2 rounded-4xl
+                transition-all ease-in-out duration-500 hover:-translate-y-1.5 cursor-pointer opacity-80
+                active:translate-0 mt-4
+              '>
+                Github
+              </button>
+            </a>
+
+            <a 
+              href="https://movix-register.vercel.app/"
               target='_blank'
               rel='noopener noreferrer'
             >
